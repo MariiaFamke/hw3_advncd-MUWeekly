60e281a9d0985692fd8dc68a362fd4f4198af112
Mariia Kolisnyk <kolesnik.masha@gmail.com>
2014-10-13 10:56:12 +0300

    Initial Commit

---
+++ MUWeekly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MUWeekly
+//
+//  Created by Дмитрий on 10/13/14.
+//  Copyright (c) 2014 Mariia Famke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
